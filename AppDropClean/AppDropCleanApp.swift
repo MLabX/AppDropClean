1065fcdbfa1faa8abb997963d4caab9020739c7d
michaelyoung <mic.z.young@gmail.com>
2025-05-21 22:52:43 +1000

    Initial Commit

---
+++ AppDropClean/AppDropCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDropCleanApp.swift
+//  AppDropClean
+//
+//  Created by Michael Young on 21/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppDropCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppDropCleanTests/AppDropCleanTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppDropCleanTests.swift
+//  AppDropCleanTests
+//
+//  Created by Michael Young on 21/5/2025.
+//
+
+import Testing
+@testable import AppDropClean
+
+struct AppDropCleanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
